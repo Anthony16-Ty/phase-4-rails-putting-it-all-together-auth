36a93946a49ede40c41dda2f0ea0014943bedbfc
Anthony Kimeu <tkymeu@gmail.com>
2022-10-17 10:23:53 +0300

    commit tree a690a591cd95163c8760b64334c5bb60aae77bac

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,5 @@
-class UsersController < ApplicationController
-    skip_before_action :authenticate, only: [:create]
-
-    def create
-        user = User.create!(user_params)
-        session[:user_id] = user.id
-        render json: user, status: :created
-    end
-
-    def show
-        user = User.find(session[:user_id])
-        render json: user, status: :created
-    end
-
-    private
-
-    def user_params
-        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
-    end
+class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true, uniqueness: {case_sensitive: true}
+    has_many :recipes
 end
